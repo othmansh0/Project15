e4790b81b9e880445ce2c487c6289fb06ae40700
Othman Shahrouri <othmanshahrouri@gmail.com>
2021-09-14 12:49:19 +0300

    Create tap button to switch animations

--- Project15/ViewController.swift
+++ Project15/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imageView: UIImageView!
+    var currentAnimation = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //There isnt an image view in storyboard so will create on here
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+        
     }
 
-
+    @IBAction func tapped(_ sender: Any) {
+        //Every time  user taps button, we're going to execute a different animation
+        currentAnimation += 1
+        
+        
+        
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
 }
 
